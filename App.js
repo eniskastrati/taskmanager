5ee5bff3145430f2cce1e2ff734c68ae03ebde34
eniskastrati <enis@smartmile.eu>
2023-09-06 15:20:33 +0200

    updating route

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import Spinner from "react-native-loading-spinner-overlay/lib";
+import { retrieveData } from './src/storage/localstorage';
+
+//importing screens
+import Setup from './src/screens/Setup';
+import Home from './src/screens/Home';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [mainRoute, setMainRoute] = useState('');
+
+  useEffect(() => {
+    getDataFromAysnc();
+  }, [])
+
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop))
+        return false;
+    }
+    return true;
+  }
+  
+  const getDataFromAysnc = async () => {
+    let data = await retrieveData();
+    if (isEmpty(data)) {
+      setMainRoute('Setup')
+    } else setMainRoute('Home')
+    setLoading(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Spinner
+          visible={loading}
+          textContent={'Loading...'}
+          cancelable={false}
+          textStyle={styles.spinnerTextStyle}
+        />
+        {mainRoute == 'Home' ? <Home /> : <Setup />}
     </View>
   );
 }
