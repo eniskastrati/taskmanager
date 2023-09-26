c26913e2a45cc2e4e42dcefcc2f01615493a6d73
eniskastrati <enis@smartmile.eu>
2023-09-26 20:35:19 +0200

    adding screen navigations on app

--- App.js
+++ App.js
@@ -8,9 +8,14 @@ import { retrieveData } from './src/storage/localstorage';
 import Setup from './src/screens/Setup';
 import Home from './src/screens/Home';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
 export default function App() {
   const [loading, setLoading] = useState(true);
-  const [mainRoute, setMainRoute] = useState('Home');
+  const [mainRoute, setMainRoute] = useState('');
 
   useEffect(() => {
     getDataFromAysnc();
@@ -28,28 +33,34 @@ export default function App() {
     let data = await retrieveData();
     console.log("Getting data from Asnyc", data)
     if (isObjEmpty(data)) {
-      setMainRoute('Setup')
-    } else setMainRoute('Home')
+      setMainRoute("Setup")
+    } else setMainRoute("Home")
+    console.log(mainRoute)
     setLoading(false);
   }
 
   return (
-    <View style={styles.container}>
-      <Spinner
-          visible={loading}
-          textContent={'Loading...'}
-          cancelable={false}
-          textStyle={styles.spinnerTextStyle}
-        />
-        {mainRoute == 'Home' ? <Home /> : <Setup />}
-    </View>
+    <NavigationContainer>
+        <Spinner
+            visible={loading}
+            textContent={'Loading...'}
+            cancelable={false}
+            textStyle={styles.spinnerTextStyle}
+          />
+          {mainRoute.length > 0 && 
+            <Stack.Navigator initialRouteName={mainRoute}>
+              <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+              <Stack.Screen name="Setup" component={Setup} options={{ headerShown: false }}/>
+            </Stack.Navigator>
+          }
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E1E21',
     alignItems: 'center',
     justifyContent: 'center',
   },
