82203ea5adec9c0b3cb88504b5e5d4ac7bfd5ac0
eniskastrati <enis@smartmile.eu>
2023-09-29 09:07:34 +0200

    updating cirle button component

--- src/components/Circlebutton.js
+++ src/components/Circlebutton.js
@@ -1,11 +1,11 @@
 import { View, Pressable, StyleSheet } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-export default function CircleButton({ onPress }) {
+export default function CircleButton({ onPress, text }) {
   return (
     <View style={styles.circleButtonContainer}>
       <Pressable style={styles.circleButton} onPress={onPress && onPress}>
-        <MaterialIcons name="add" size={38} color="#25292e" />
+        <MaterialIcons name={text} size={38} color="#25292e" />
       </Pressable>
     </View>
   );
@@ -13,19 +13,28 @@ export default function CircleButton({ onPress }) {
 
 const styles = StyleSheet.create({
   circleButtonContainer: {
-    width: 84,
-    height: 84,
+    width: 80,
+    height: 80,
     marginHorizontal: 60,
-    borderWidth: 4,
+    borderWidth: 2.5,
     borderColor: '#3880ff',
     borderRadius: 42,
     padding: 3,
+    margin: 10,
+    shadowColor: "#3880ff",
+    shadowOffset: {
+      width: -1,
+      height: -10
+    },
+    shadowOpacity: 0.55,
+    shadowRadius: 15,
+    elevation: 10000
   },
   circleButton: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 42,
-    backgroundColor: '#fff',
+    backgroundColor: '#F2F2F2',
   },
 });
